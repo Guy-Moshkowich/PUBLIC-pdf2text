6de358fef34d2bfdbcc419c34bcedc3476c3a67a
Guy-Moshkowich <guy.moshkowich@gmail.com>
2020-05-01 13:09:52 +0300

    clean-upå

--- pdf_to_images.py
+++ pdf_to_images.py
@@ -12,6 +12,3 @@ def extract_doc_images(pdf_path):
             image.save(folder_image + '/' + str(cnt) + '.jpg', 'JPEG')
 
 
-if __name__ == '__main__':
-    extract_doc_images('.', 'pharmaceutical-development-con-clo-sys.pdf');
-
